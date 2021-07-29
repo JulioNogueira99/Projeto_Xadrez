6028dfec760cdf0ef1b57de6f69bc857169631b8
Julio Nogueira <julio.nogueira561@gmail.com>
2021-07-29 16:42:19 -0300

    Acrescentando as demais pecas

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -36,7 +36,7 @@ namespace tabuleiro
             return false;
         }
 
-        public bool PoderMoverPara(Posicao pos)
+        public bool MovimentoPossivel(Posicao pos)
         {
             return MovimentosPossiveis()[pos.Linha, pos.Coluna];
         }
